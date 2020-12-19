d5e4a693dfdc6eba8ccc0407ac7cdb3e9e416873
RajatArya007zxc <rajatarya199zx@gmail.com>
2020-12-19 12:42:56 +0530

    the setup is intialize

---
+++ src/Component/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Sidebar.css'
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h1>Sdiebar</h1>
+        </div>
+    )
+}
---
+++ src/Firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD8_xIS3JhyRhybngFPVThs_3GKqhb3zeA",
+    authDomain: "whatsapp-mern-e1b6e.firebaseapp.com",
+    projectId: "whatsapp-mern-e1b6e",
+    storageBucket: "whatsapp-mern-e1b6e.appspot.com",
+    messagingSenderId: "242371588259",
+    appId: "1:242371588259:web:b42a44a71627563af30f16",
+    measurementId: "G-K52QKK2YX0"
+  };
